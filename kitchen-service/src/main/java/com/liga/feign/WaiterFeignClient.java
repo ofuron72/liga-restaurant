eb3057c5894d4131e0dc10e2ce1f312c1739ac5e
ofuron72 <pereligin.3004@gmail.com>
2025-04-09 22:43:49 +0300

    feat(lesson-6):fix url path

--- kitchen-service/src/main/java/com/liga/feign/WaiterFeignClient.java
+++ kitchen-service/src/main/java/com/liga/feign/WaiterFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "waiter-service", url = "localhost:8081")
+@FeignClient(value = "waiter-service", url = "${spring.cloud.openfeign.client.config.waiter-service.url}")
 public interface WaiterFeignClient {
     @PostMapping("/integration-api/orders/cooked")
     ResponseEntity<Void> sendCookedOrderToWaiter(@RequestBody WaiterOrderRequestDto waiterOrderRequestDto);
--- waiter-service/src/main/java/com/liga/integration/feign/KitchenFeignClient.java
+++ waiter-service/src/main/java/com/liga/integration/feign/KitchenFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "kitchen-service", url = "localhost:8080")
+@FeignClient(value = "kitchen-service", url = "${spring.cloud.openfeign.client.config.kitchen-service.url}")
 public interface KitchenFeignClient {
 
     @PostMapping("/integration-api/orders")
