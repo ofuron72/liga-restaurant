db517f4d9858760ea22cb9f491f0c136ec04297b
ofuron72 <pereligin.3004@gmail.com>
2025-04-22 14:05:39 +0300

    feat(lesson-9):add validation

--- waiter-service/src/main/java/com/liga/dto/DishSendDto.java
+++ waiter-service/src/main/java/com/liga/dto/DishSendDto.java
@@ -1,6 +1,8 @@
 package com.liga.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 /**
  * Представляет информацию о блюде, включённом в заказ,
@@ -8,6 +10,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public record DishSendDto(
         @JsonProperty("shortName") String shortName,
+        @Positive(message = "dishes number must be positive")
+        @NotNull(message = "dishes number must not be null")
         @JsonProperty("dishesNumber") Long dishesNumber
 ) {
 }
--- waiter-service/src/main/java/com/liga/dto/WaiterOrderCreateRequestDto.java
+++ waiter-service/src/main/java/com/liga/dto/WaiterOrderCreateRequestDto.java
@@ -1,5 +1,6 @@
 package com.liga.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
@@ -12,7 +13,7 @@ public record WaiterOrderCreateRequestDto(@NotNull(message = "waiterId must not 
                                           @NotNull(message = "tableNo must not be null")
                                           String tableNo,
                                           @Size(min = 1, message = "count of dishes must be at least 1")
-                                          List<DishSendDto> dishes
+                                          List<@Valid DishSendDto> dishes
 ) {
 
 }
